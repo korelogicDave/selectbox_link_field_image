9774eaf13591cf598bccfac8d20e93e56819d4d0
Michael Hay <michael.hay@korelogic.co.uk>
2015-08-11 07:31:28 +0100

    Initial Commit

---
+++ content/content.index.php
@@ -0,0 +1,34 @@
+<?php
+
+
+	require_once(TOOLKIT.'/class.administrationpage.php');
+
+
+
+	class contentExtensionSelectbox_link_field_imageIndex extends AdministrationPage
+	{
+
+		public function view(){
+
+			// Get the sorting order:
+			if( isset($_GET['get']) ){
+				$id = intval($_GET['get']);
+				if( $id == '' ){
+					$id = 0;
+				}
+
+				$order = Symphony::Database()->fetchVar('related_field_id', 0, 'SELECT `related_field_id` FROM `tbl_sblp_sortorder` WHERE `entry_id` = '.$id);
+
+				if( $order !== false ){
+					$order = json_encode(unserialize($order));
+				}
+				else{
+					$order = "{}";
+				}
+
+				echo $order;
+			}
+
+			die();
+		}
+	}
